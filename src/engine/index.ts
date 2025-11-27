70eac92834fe2e2255c393a7164dba307ca432a2
whisleragentdarmon-gif <whisler.agent.darmon@gmail.com>
2025-11-27 18:03:58 +0100

    Update index.ts

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,12 +1,17 @@
-
 import { MatchPredictor } from './MatchPredictor';
 import { ComboGenerator } from './ComboGenerator';
 import { LiveEngine } from './LiveEngine';
 import { BankrollManager } from './BankrollManager';
 
+// 👉 Ajout essentiel
+import { MOCK_MATCHES } from '../constants';
+
 export const OracleAI = {
   predictor: new MatchPredictor(),
   combo: new ComboGenerator(),
   live: new LiveEngine(),
-  bankroll: new BankrollManager()
+  bankroll: new BankrollManager(),
+
+  // 👉 Export pour faciliter l'accès global
+  mocks: MOCK_MATCHES
 };
