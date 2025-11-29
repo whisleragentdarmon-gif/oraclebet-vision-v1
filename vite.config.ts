83a66385fa7c2dcfb8b5b3b2d75270f3f2456d07
whisleragentdarmon-gif <whisler.agent.darmon@gmail.com>
2025-11-29 16:09:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,13 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    // Ne PAS mettre base: './' si vous utilisez react-router et vercel rewrites
-    // base: '/', // Valeur par défaut, c'est ce qu'on veut avec les rewrites
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    plugins: [react()],
-    define: {
-      'process.env': JSON.stringify(env)
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      }
-    },
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets', // Dossier par défaut pour les js/css
-      sourcemap: true,
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  // On s'assure que le serveur écoute bien partout (utile pour le debug)
+  server: {
+    host: true
+  }
 });
