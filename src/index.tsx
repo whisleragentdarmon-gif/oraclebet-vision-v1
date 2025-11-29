6d9b3ef41bf0134915bb638fdbbf9fa4617a4d80
whisleragentdarmon-gif <whisler.agent.darmon@gmail.com>
2025-11-29 17:07:38 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-// Fichier : src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.css'; // C'est lui qui charge le design !
 
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Root element not found");
+
+if (!rootElement) {
+  throw new Error("Impossible de trouver l'élément root");
+}
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
